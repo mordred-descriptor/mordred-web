d6934ebf6625b4f79157b733678c3e03bca3f64c
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-06-22 07:15:03 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mordred-web',
     version='0.1.0',
-    packages=[
-        'mordred_web',
-    ],
-    package_dir={'mordred_web': 'python/mordred_web'},
-
-    install_requires=[
-        'mordred>=0.2.1',
-        'tornado>=4.4.2',
-        'psutil>=5.0.1',
-    ],
-
+    description='molecular descriptor calculator web UI',
+    license='BSD3',
     author='Hirotomo Moriwaki',
     author_email='philopon.dependence@gmail.com',
-    license='BSD3',
+    url='https://github.com/mordred-descriptor/mordred-web',
+    platforms=['any'],
+    keywords='QSAR chemoinformatics',
+    packages=[
+        'mordred.web',
+        'mordred.web.handler',
+    ],
+    package_dir={
+        'mordred.web': 'python/mordred_web',
+        'mordred.web.handler': 'python/mordred_web/handler',
+    },
+
+    package_data={
+        'mordred.web': [
+            'static/*',
+            'static/**/*',
+        ],
+    },
+
+    install_requires=[
+        'mordred>=0.3.1',
+        'tornado>=4.5',
+        'psutil>=5.0',
+        'base58>=0.2.0',
+        'numpy>=1.10',
+        'six>=1.10',
+    ],
+
 )
