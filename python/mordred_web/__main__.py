1cbbe947085cda7b76025ad0d07568536fab8260
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-06-30 03:48:15 +0900

    add command line parameter (closed #1)

--- python/mordred_web/__main__.py
+++ python/mordred_web/__main__.py
@@ -1,6 +1,10 @@
 import os
 
 import tornado.web
+import socket
+import argparse
+import psutil
+import webbrowser
 
 from .db import connect
 from .task_queue import TaskQueue
@@ -29,10 +33,19 @@ class MyApplication(tornado.web.Application):
         self.calc_timeout = calc_timeout
 
 
-def main(port, workers,
-         file_size_limit=3, molecule_limit=50,
-         parse_timeout=60, prepare_timeout=60, calc_timeout=60,
-         db='mordred-web.sqlite'):
+def serve(port, workers, no_browser,
+          file_size_limit=3, molecule_limit=50,
+          parse_timeout=60, prepare_timeout=60, calc_timeout=60,
+          db='mordred-web.sqlite'):
+
+    if port is None:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('', 0))
+            port = s.getsockname()[1]
+
+    if workers is None:
+        workers = psutil.cpu_count(logical=False)
+
     static = os.path.join(os.path.dirname(__file__), 'static')
     ioloop = tornado.ioloop.IOLoop.current()
 
@@ -57,9 +70,48 @@ def main(port, workers,
         server = tornado.httpserver.HTTPServer(app, max_body_size=(file_size_limit + 1) * MEGA)
         server.bind(port)
         server.start(1)
-        print('start mordred.web on localhost:{}'.format(port))
+        url = "http://127.0.0.1:{}".format(port)
+        if not no_browser:
+            webbrowser.open(url, autoraise=True)
+        print('start mordred.web on {}'.format(url))
         ioloop.start()
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Mordred Web UI')
+    parser.add_argument('-p', '--port', type=int, default=None, help='port')
+    parser.add_argument('-w', '--workers', type=int, default=None, help='number of workers')
+    parser.add_argument(
+        '--file-size-limit', metavar="MB", type=int, default=5,
+        help="upload file size limit",
+    )
+    parser.add_argument(
+        '--molecule-limit', metavar="N", type=int, default=None,
+        help="number of molecule limit",
+    )
+    parser.add_argument(
+        '--parse-timeout', metavar='SEC', type=int, default=None,
+        help="file parse timeout",
+    )
+    parser.add_argument(
+        '--prepare-timeout', metavar="SEC", type=int, default=None,
+        help="molecular preparation timeout",
+    )
+    parser.add_argument(
+        '--calc-timeout', metavar="SEC", type=int, default=None,
+        help="descriptor calculation timeout",
+    )
+    parser.add_argument(
+        '--db', metavar="FILE", type=str, default="mordred-web.sqlite",
+        help="database file path"
+    )
+    parser.add_argument(
+        '--no-browser', action='store_true', default=False,
+        help="don't open browser automatically"
+    )
+    result = (parser.parse_args())
+    serve(**vars(result))
+
+
 if __name__ == "__main__":
-    main(3000, 2)
+    main()
