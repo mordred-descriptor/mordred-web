58c719e11412ab118f0debfd46004a58ad946eaa
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-06-30 13:30:29 +0900

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mordred-web',
-    version='0.2.0',
+    version='0.2.1',
     description='molecular descriptor calculator web UI',
     license='BSD3',
     author='Hirotomo Moriwaki',
